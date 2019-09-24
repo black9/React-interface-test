2402d507a0a22aa7eec2620c904cb6f516290379
Kim <csu02049@naver.com>
2019-09-25 02:34:07 +0900

    dialog test

--- screens/MyInvest.js
+++ screens/MyInvest.js
@@ -2,14 +2,54 @@ import React, { Component } from "react";
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    TouchableOpacity
 } from "react-native";
+import {
+    MaterialDialog,
+} from 'react-native-material-dialog';
+import { material } from 'react-native-typography';
 
-class MyInvest extends Component {
+
+class MyInvest extends Component{
+
+    state ={
+        basicCustomColorsVisible: false,
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text> Empty </Text>
+                
+                <TouchableOpacity
+                                onPress={() =>
+                                    this.setState({ basicCustomColorsVisible: true })
+                                }
+                            >
+                                <View style={styles.button}>
+                                    <Text style={material.button}>이것은 테스트</Text>
+                                </View>
+                            </TouchableOpacity>
+
+                <MaterialDialog
+                    visible={this.state.basicCustomColorsVisible}
+                    title={'돈 빌릴래?'}
+                    titleColor="#F0F0F0"
+                    colorAccent="#6ABED0"
+                    backgroundColor="#181712"
+                    okLabel="응"
+                    onOk={() => {
+                        this.setState({ basicCustomColorsVisible: false });
+                    }}
+                    cancelLabel="아니"
+                    onCancel={() => {
+                        this.setState({ basicCustomColorsVisible: false });
+                    }}
+                >
+                    <Text style={[material.subheading, { color: '#B0ABA0' }]}>
+                        주의사항
+              </Text>
+                </MaterialDialog>
             </View>
         );
     }
